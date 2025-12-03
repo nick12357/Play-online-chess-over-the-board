b56a6c8c10dae4b2326f79aa49077bffbffdbec5
nick12357 <101987755+nick12357@users.noreply.github.com>
2025-12-02 19:45:55 -0500

    make basic version

--- Lichess api.py
+++ Lichess api.py
@@ -1,1 +1,14 @@
+import requests
+
 api=input("lichess api")
+move=input("move in uci format")
+requests.post(
+    "https://lichess.org/api/board/game/5IrD6Gzz/move/",move,
+    headers={
+      "Authorization": api
+    },
+    params={
+      "offeringDraw": "false"
+    }
+
+)
