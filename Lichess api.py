81dd5ca0c6a529365156bdca17a6efcc4e23ff44
Your Name <neekanhaghiri@gmail.com>
2025-12-12 14:47:13 -0500

    OfferingDraw=false

--- Lichess api.py
+++ Lichess api.py
@@ -1,14 +1,1 @@
-import requests
-
-api=input("lichess api")
-move=input("move in uci format")
-requests.post(
-    "https://lichess.org/api/board/game/5IrD6Gzz/move/",move,
-    headers={
-      "Authorization": api
-    },
-    params={
-      "offeringDraw": "false"
-    }
-
-)
+print("work in progress")
--- send.sh
+++ send.sh
@@ -1,8 +1,7 @@
 read api
 read move
 read board_id
-
-
-curl "https://lichess.org/api/board/game/$board_id/move/$move?offeringDraw=true" \
+                ########################################################################################################################
+curl "https://lichess.org/api/board/game/$board_id/move/$move?offeringDraw=false" \
   --request POST \
   --header "Authorization: Bearer $api"
