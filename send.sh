01bf8ba95b9f05e3ab9e5204ed722fdc47eb276d
Your Name <neekanhaghiri@gmail.com>
2025-12-12 15:22:44 -0500

    Add prompts for user input.

--- send.sh
+++ send.sh
@@ -1,6 +1,7 @@
-read api
-read move
-read board_id
+read -p "whats the api key" api
+read -p "whats your move" move
+read -p "whats the board id" board_id
+
                 ########################################################################################################################
 curl "https://lichess.org/api/board/game/$board_id/move/$move?offeringDraw=false" \
   --request POST \
