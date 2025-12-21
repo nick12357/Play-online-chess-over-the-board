f50a9b0d9e1163db7352a6ea1f66b4b85e0846e0
nick12357 <101987755+nick12357@users.noreply.github.com>
2025-12-21 13:19:19 -0500

    Delete send.sh

--- send.sh
+++
@@ -1,8 +0,0 @@
-read -p "whats the api key" api
-read -p "whats your move" move
-read -p "whats the board id" board_id
-
-                ########################################################################################################################
-curl "https://lichess.org/api/board/game/$board_id/move/$move?offeringDraw=false" \
-  --request POST \
-  --header "Authorization: Bearer $api"
